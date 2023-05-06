d470e0a746981027264ed5dae5adeb0b8f584725
AKot1988 <kulibabenko@ukr.net>
2023-05-06 16:46:46 +0300

    4-th homework resolve via array methods

--- 4-th homework arrays/4-th homework arrays.js
+++ 4-th homework arrays/4-th homework arrays.js
@@ -1,32 +1,32 @@
-// ## TASK-01
+exampleArray = [3, 4, 5, 7, 9, 10, 11, 12];
+let arr1 = [1, 3, 5, 7, 9, 11, 12];
+let arr2 = [1, 2, 3, 4, 5, 10, 12];
 
-let fill = (arraySize, value) => {
+// ## TASK-01
+const fill = (arraySize, value) => {
   let newArray = [];
-  for (let i = 1; i <= arraySize; i++) {
-    newArray.push(value);
-  }
+  newArray.length = arraySize;
+  newArray.fill(value);
   return newArray;
 };
 
-// ## TASK-02
+console.log(fill(15, 21));
 
-let reverseArray = (array) => {
+// ## TASK-02
+const reverseArray = (array) => {
   let reversedArray = [];
-  for (let i = array.length - 1; i >= 0; i--) {
-    reversedArray.push(array[i]);
-  }
+  array.map((el) => reversedArray.unshift(el));
   return reversedArray;
 };
+console.log(reverseArray(exampleArray));
 
 // ## TASK-03
-
-let mergeArrays = (firstArray, secondArray) => {
-  let mergedArray = firstArray;
-  for (let i = 0; i < secondArray.length; i++) {
-    if (!firstArray.includes(secondArray[i])) {
-      mergedArray.push(secondArray[i]);
-    }
-  }
-  mergedArray.sort((a, b) => a - b);
-  return mergedArray;
+const mergeArrays = (firstArray, secondArray) => {
+  mergedUnfilteredArray = firstArray.concat(secondArray);
+  mergedFilteredArray = mergedUnfilteredArray.filter(
+    (el, id) => mergedUnfilteredArray.indexOf(el) === id
+  );
+  mergedFilteredArray.sort((a, b) => a - b);
+  return mergedFilteredArray;
 };
+console.log(mergeArrays(arr1, arr2));
